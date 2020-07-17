0e3af1f62055fdb643ea1ba6c133654b9c32c1ec
kathrinjansen <kathrin.jansen@kennedy.ox.ac.uk>
2020-07-17 14:26:25 +0100

    fix issue that scvelo was not waiting for phate to finish

--- pipelines/pipeline_seurat.py
+++ pipelines/pipeline_seurat.py
@@ -1367,7 +1367,7 @@ RDIMS_VIS_COMP_2 = "UMAP_2"
 # ########################################################################### #
 
 @active_if(PARAMS["run_velocity"])
-@follows(paga)
+@follows(paga, phate)
 @transform(RDIMS_VIS_TASK,
            regex(r"(.*)/(.*)/(.*).sentinel"),
            r"\1/velocity.dir/scvelo.sentinel")
